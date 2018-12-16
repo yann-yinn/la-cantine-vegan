28d48d0a5913bd453d957bb047c431252e44b61e
yann <yann.boisselier@gmail.com>
2018-12-16 13:13:14 +0100

    ultime mail test

---
+++ functions-src/mail.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+import sgMail from '@sendgrid/mail'
+
+exports.handler = async (event, context) => {
+  // using SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  console.log("hello " + process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'yann@yineo.fr',
+    from: 'yann.boisselier@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+
+  return {
+    statusCode: 200,
+    body: 'OK'
+  };
+};
