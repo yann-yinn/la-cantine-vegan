bd821f45f0bdee1898bf31fcdbd92b4350297b0c
yann <yann.boisselier@gmail.com>
2018-12-16 10:51:25 +0100

    try to fix import

--- functions/mail.js
+++ functions/mail.js
@@ -1,7 +1,8 @@
+import sgMail from '@sendgrid/mail'
+
 exports.handler = async (event, context) => {
   // using SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
-  const sgMail = require('@sendgrid/mail');
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: 'yann@yineo.fr',
