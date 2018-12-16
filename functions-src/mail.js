dae7a3af2ccf7319fd8a2ed3c3ab6908764edc4c
yann <yann.boisselier@gmail.com>
2018-12-16 13:58:11 +0100

    fix code for mail

--- functions-src/mail.js
+++ functions-src/mail.js
@@ -1,26 +1,23 @@
 import sgMail from '@sendgrid/mail'
 
-exports.handler = async (event, context) => {
+exports.handler = (event, context, callback) => {
   // using SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
-  console.log("==== DEBUG" + process.env.SENDGRID_API_KEY + "=====")
+  console.log("DEBUG :" + process.env.SENDGRID_API_KEY)
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: 'yann@yineo.fr',
     from: 'yann.boisselier@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
+    subject: 'Sending with SendGrid is Fun from localhost',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
   sgMail.send(msg).then(r => {
-    return {
+    callback(null, {
       statusCode: 200,
       body: 'OK'
-    }
+    })
   }).catch(e => {
-    return {
-      statusCode: 500,
-      body: e
-    }
+    callback(e)
   })
 };
